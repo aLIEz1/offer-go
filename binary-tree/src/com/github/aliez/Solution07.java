17cdaf8bb5c4a657c41df671201aae5e363a68c5
lm <super1742720898@gmail.com>
2021-07-19 11:22:47 +0800

    07 buildTree

--- binary-tree/src/com/github/aliez/Solution07.java
+++ binary-tree/src/com/github/aliez/Solution07.java
@@ -1,6 +1,9 @@
 package com.github.aliez;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 07 重建二叉树
  *
@@ -8,6 +11,13 @@ package com.github.aliez;
  * @date 2021/7/18 23:36
  */
 public class Solution07 {
+    /**
+     * 涉及数组拷贝，时间复杂度太高，转用索引法
+     *
+     * @param preorder 前序遍历结果
+     * @param inorder  中序遍历结果
+     * @return 根节点
+     */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         if (preorder.length == 0) {
             return null;
@@ -18,7 +28,6 @@ public class Solution07 {
         if (preorder.length == 1) {
             return root;
         }
-
         int delimiterIndex;
         for (delimiterIndex = 0; delimiterIndex < inorder.length; delimiterIndex++) {
             if (inorder[delimiterIndex] == rootValue) {
@@ -39,6 +48,31 @@ public class Solution07 {
 
         root.left = buildTree(preLeft, inLeft);
         root.right = buildTree(preRight, inRight);
+        return root;
+
+    }
+
+    private Map<Integer, Integer> indexMap;
+
+    public TreeNode buildTree1(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        indexMap = new HashMap<>(16);
+        for (int i = 0; i < inorder.length; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+
+    private TreeNode myBuildTree(int[] preorder, int[] inorder, int preLeft, int pRight, int inLeft, int inRight) {
+        if (preLeft > pRight) {
+            return null;
+        }
+        int rootValue = preorder[preLeft];
+        int delimiterIndex = indexMap.get(rootValue);
+        int leftSize = delimiterIndex - inLeft;
+        TreeNode root = new TreeNode(rootValue);
+        root.left = myBuildTree(preorder, inorder, preLeft + 1, preLeft + leftSize, inLeft, delimiterIndex - 1);
+        root.right = myBuildTree(preorder, inorder, preLeft + 1 + leftSize, pRight, delimiterIndex + 1, inRight);
         return root;
 
     }
